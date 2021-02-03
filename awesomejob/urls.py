0994a58afed2e9b8a1f07f2827e738b876e82a57
trisduong <trisduong.tpi@gmail.com>
2021-02-04 00:30:29 +0700

    update

--- awesomejob/urls.py
+++ awesomejob/urls.py
@@ -5,10 +5,10 @@ from .views import dynamic_lookup_job_view
 
 urlpatterns = [
     path('', views.index),
-    path('familug/', views.fami),
+    path('familug/new', views.fami),
     path('awesomejob/', views.jobs),
     path('awesomejob/<int:my_id>/', dynamic_lookup_job_view, name='awesome job'),
-    path('python_fami/', views.python_fami),
-    path('sysadmin_fami/', views.sys_fami),
-    path('command_fami/', views.com_fami),
+    path('familug/python', views.python_fami),
+    path('familug/sysadmin', views.sys_fami),
+    path('familug/command', views.com_fami),
 ]
